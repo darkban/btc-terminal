29dd31c6c4551658b44ca8d6d3c8dba17d0f3733
darkban <32014414+darkban@users.noreply.github.com>
2019-10-16 19:04:25 +0200

    Update buttonrow.js

--- components/buttonrow.js
+++ components/buttonrow.js
@@ -3,8 +3,8 @@ import Button from 'preact-material-components/Button'
 const Buttonrow = ({cancel, confirm}) => {
     return (
         <div class='buttons'>
-            <Button onClick={cancel}>Cancel</Button>
-            <Button raised onClick={confirm}>Ok</Button>
+            <Button onClick={cancel}>Cancella</Button>
+            <Button raised onClick={confirm}>OK</Button>
         </div>
     )
 }
