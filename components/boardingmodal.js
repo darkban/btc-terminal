f17dcdbe4c491102195bb032b9439db2b02c5ab9
darkban <32014414+darkban@users.noreply.github.com>
2019-10-17 10:27:27 +0200

    Update boardingmodal.js

--- components/boardingmodal.js
+++ components/boardingmodal.js
@@ -5,7 +5,7 @@ import TextField from 'preact-material-components/TextField'
 const BoardingModal = ({open, change, click, url}) => {    
     return (
         <Dialog class={open ? 'mdc-dialog--open' : ''} onAccept={click}>
-            <Dialog.Header>Initial setup</Dialog.Header>
+            <Dialog.Header>INSTALLAZIONE</Dialog.Header>
             <Dialog.Body>
               <TextField fullwidth type='url' placeholder="URL CryptoLocalPOS" onChange={change} value={url}/>             
             </Dialog.Body>
